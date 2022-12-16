b77573d08c6985f4d5fd30a7109455cf545152a7
Shabrina <76257726+shabrina438@users.noreply.github.com>
2022-12-16 23:28:27 +0600

    Create App.vue
    add

---
+++ Vue/vue-task/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<Header_v/>
+<router-view />
+</template>
+
+<script>
+import Header_v from './components/Header_v.vue'
+export default {
+  name: 'App',
+  components:{
+    Header_v,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body{
+  padding: 0;
+  margin: 0;
+}
+</style>
